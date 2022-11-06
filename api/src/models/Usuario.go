38cc1a720b0036cf4735bbb862906693c89afb00
leoneville <leoneville_@hotmail.com>
2022-11-06 20:40:55 -0300

    feat: Adicionando hash no cadastro de senha do usuario

--- api/src/models/Usuario.go
+++ api/src/models/Usuario.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"strings"
 	"time"
@@ -24,7 +25,9 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return err
 	}
 
-	usuario.formatar()
+	if err := usuario.formatar(etapa); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -52,8 +55,17 @@ func (usuario *Usuario) validar(etapa string) error {
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+	if strings.ToLower(etapa) == "cadastro" {
+		senhaComHash, err := seguranca.Hash(usuario.Senha)
+		if err != nil {
+			return err
+		}
+		usuario.Senha = string(senhaComHash)
+	}
+
+	return nil
 }
