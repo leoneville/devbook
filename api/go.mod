9172af7b330c0a74e7564a05d562f5fd6c2ce42b
leoneville <leoneville_@hotmail.com>
2022-11-12 14:00:57 -0300

    feat: adicionando pacote de autenticacao e criacao de token

--- api/go.mod
+++ api/go.mod
@@ -11,3 +11,5 @@ require github.com/go-sql-driver/mysql v1.6.0
 require github.com/badoux/checkmail v1.2.1
 
 require golang.org/x/crypto v0.1.0
+
+require github.com/golang-jwt/jwt/v4 v4.4.2
--- api/src/controllers/login.go
+++ api/src/controllers/login.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"api/src/autenticacao"
 	"api/src/banco"
 	"api/src/models"
 	"api/src/repositorios"
@@ -45,5 +46,6 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Você está logado!"))
+	token, _ := autenticacao.CriarToken(usuarioSalvoNoBanco.ID)
+	w.Write([]byte(token))
 }
