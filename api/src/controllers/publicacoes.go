a38e204dc280d7bfcf438fcd6f7a7dedcf4d90b5
leoneville <leoneville_@hotmail.com>
2022-11-22 13:27:39 -0300

    feat: adicionando rota criar publicacao

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -1,10 +1,52 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 // CriarPublicacao adiciona uma nova publicação no banco de dados
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, err := autenticacao.ExtrairUsuarioID(r)
+	if err != nil {
+		respostas.Erro(w, http.StatusUnauthorized, err)
+		return
+	}
 
+	corpoRequisicao, err := io.ReadAll(r.Body)
+	if err != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var publicacao models.Publicacao
+	if err = json.Unmarshal(corpoRequisicao, &publicacao); err != nil {
+		respostas.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+	publicacao.AutorID = usuarioID
+
+	db, err := banco.Conectar()
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, err = repositorio.Criar(publicacao)
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
 }
 
 // BuscarPublicacoes traz as publicações que apareceriam no feed do usuário
