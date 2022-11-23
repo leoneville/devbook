9ab59f1d259f2ffa78ceb0b30ae028c79b845857
leoneville <leoneville_@hotmail.com>
2022-11-23 11:24:14 -0300

    feat: adicionando rota buscarpublicacao

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -36,3 +36,35 @@ func (repositorio publicacoes) Criar(publicacao models.Publicacao) (uint64, erro
 
 	return uint64(ultimoIDInserido), nil
 }
+
+// BuscarPorID traz uma única publicação do banco de dados
+func (repositorio publicacoes) BuscarPorID(publicacaoID uint64) (models.Publicacao, error) {
+	linha, err := repositorio.db.Query(`
+		SELECT p.*, u.nick from
+		publicacoes p inner join usuarios u
+		on u.id = p.autor_id where p.id = ?`,
+		publicacaoID,
+	)
+	if err != nil {
+		return models.Publicacao{}, err
+	}
+	defer linha.Close()
+
+	var publicacao models.Publicacao
+
+	if linha.Next() {
+		if err = linha.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadaEm,
+			&publicacao.AutorNick,
+		); err != nil {
+			return models.Publicacao{}, err
+		}
+	}
+
+	return publicacao, nil
+}
