be42bd8b3e7fae08ff668919895eaddd1cb70e14
leoneville <leoneville_@hotmail.com>
2022-11-28 08:10:59 -0300

    feat: configurando pagina de login

--- webapp/main.go
+++ webapp/main.go
@@ -5,11 +5,14 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/router"
+	"webapp/src/utils"
 )
 
 func main() {
-	fmt.Println("Rodando WebApp!")
-
+	utils.CarregarTemplates()
 	r := router.Gerar()
+
+	fmt.Println("Escutando na porta 3000...")
+	fmt.Println("\nHost -> http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
