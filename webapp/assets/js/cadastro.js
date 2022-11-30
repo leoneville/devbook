7283e78dd5ed7fdb67be0ec6ea21e240ef4f456d
leoneville <leoneville_@hotmail.com>
2022-11-30 12:50:50 -0300

    feat: criando e configurando o pacote respostas

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -17,5 +17,9 @@ function criarUsuario(evento) {
             nick: $('#nick').val(),
             senha: $('#senha').val(),
         }
+    }).done(function() {
+        alert("Usuário cadastrado com sucesso!")
+    }).fail(function(erro) {
+        console.log(erro)
     });
 }
---
+++ webapp/src/respostas/respostas.go
@@ -0,0 +1,31 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// ErroAPI representa a respsta de erro da API
+type ErroAPI struct {
+	Erro string `json:"erro"`
+}
+
+// JSON  retorna uma resposta
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
+	w.Header().Set("Content-Type", "Application/Json")
+	w.WriteHeader(statusCode)
+
+	if dados != nil {
+		if err := json.NewEncoder(w).Encode(dados); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// TrataStatusCodeDeErro trata as requisições com status code 400 ou superior
+func TratarStatusCodeDeErro(w http.ResponseWriter, r *http.Response) {
+	var err ErroAPI
+	json.NewDecoder(r.Body).Decode(&err)
+	JSON(w, r.StatusCode, err)
+}
