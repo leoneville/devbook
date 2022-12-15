2d6f974d4f4ce78792cbdf058b42d45d46ff527b
leoneville <leoneville_@hotmail.com>
2022-12-15 15:14:24 -0300

    feat: criando funcionalidade para descurtir publicacoes

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -1,5 +1,7 @@
 $('#nova-publicacao').on('submit', criarPublicacao);
-$('.curtir-publicacao').on('click', curtirPublicacao);
+
+$(document).on('click', '.curtir-publicacao', curtirPublicacao)
+$(document).on('click', '.descurtir-publicacao', descurtirPublicacao)
 
 function criarPublicacao(evento) {
     evento.preventDefault();
@@ -34,11 +36,44 @@ function curtirPublicacao(evento) {
         const quatidadeDeCurtidas = parseInt(contadorDeCurtidas.text())
 
         contadorDeCurtidas.text(quatidadeDeCurtidas + 1)
+
+        elementoClicado.addClass('descurtir-publicacao')
+        elementoClicado.addClass('text-danger')
+        elementoClicado.removeClass('curtir-publicacao')
+
+
     }).fail(function() {
         alert("Erro ao curtir publicacao")
     }).always(function() {
         elementoClicado.prop('disabled', false);
     })
+}
 
-    
+function descurtirPublicacao(evento) {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+    elementoClicado.prop('disabled', true);
+
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/descurtir`,
+        method: "POST",
+    }).done(function() {
+        const contadorDeCurtidas = elementoClicado.next('span')
+        const quatidadeDeCurtidas = parseInt(contadorDeCurtidas.text())
+
+        contadorDeCurtidas.text(quatidadeDeCurtidas - 1)
+
+        elementoClicado.addClass('curtir-publicacao')
+        elementoClicado.removeClass('text-danger')
+        elementoClicado.removeClass('descurtir-publicacao')
+
+
+    }).fail(function() {
+        alert("Erro ao descurtir publicacao")
+    }).always(function() {
+        elementoClicado.prop('disabled', false);
+    })
 }
--- webapp/src/controllers/publicacoes.go
+++ webapp/src/controllers/publicacoes.go
@@ -66,3 +66,28 @@ func CurtirPublicacao(w http.ResponseWriter, r *http.Request) {
 
 	respostas.JSON(w, response.StatusCode, nil)
 }
+
+// DescurtirPublicacao chama a API para descurtir uma publicação
+func DescurtirPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, err := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if err != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: err.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicacoes/%d/descurtir", config.APIURL, publicacaoID)
+	response, err := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, nil)
+	if err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: err.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	respostas.JSON(w, response.StatusCode, nil)
+}
